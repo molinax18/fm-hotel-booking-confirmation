7be8cdb19f977c621efc8cb14f975dbb95170663
molinax18 <arielmolina8953@gmail.com>
2026-07-23 04:03:36 -0300

    chore: data type of svg plugin created in vite-env.d and plugin initialized

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    svgr(),
   ],
   resolve: {
     alias: {
