c39eac281e3d851b211caa4fc7bad783967f40f6
molinax18 <arielmolina8953@gmail.com>
2026-07-25 13:43:35 -0300

    feat(style): hover transition in App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import WelcomeSection from "./components/WelcomeSection";
 import GuideCards from "./components/GuideCards";
 import Sidebar from "./components/Sidebar";
 
+import sparkleIcon from "@/assets/icon-sparkle.svg";
+import illustrationSun from "@/assets/illustration-sun.svg";
+
 export default function App() {
   const isDesktop = useMediaQuery("(min-width: 1280px)");
 
@@ -18,10 +21,36 @@ export default function App() {
       <main className="flex flex-col gap-y-18 p-6 xl:col-start-3 xl:col-span-full xl:py-8">
         <WelcomeSection />
 
-        <section className="space-y-8 lg:space-y-0 lg:flex lg:gap-x-2 lg:max-w-3xl lg:mx-auto xl:max-w-4xl">
-          <WelcomeCard className="rotate-2 lg:rotate-4 lg:order-1 lg:flex-1" />
+        <section className="flex flex-col gap-y-4 group lg:max-w-3xl lg:mx-auto xl:max-w-4xl">
+          <div className="space-y-8 lg:relative lg:space-y-0 lg:flex lg:gap-x-2">
+            <WelcomeCard className="rotate-2 lg:rotate-4 lg:order-1 lg:flex-1 xl:group-hover:translate-x-24 xl:group-hover:-rotate-4 lg:transition-transform duration-300" />
 
-          <PaymentCard className="-rotate-2 lg:-rotate-4 lg:flex-1" />
+            {isDesktop && (
+              <img
+                src={illustrationSun}
+                alt="A illustration sun image"
+                className="absolute right-[41%] top-[60%] size-40 -z-10 xl:group-hover:-translate-y-40 lg:transition-transform duration-300"
+              />
+            )}
+
+            <PaymentCard className="-rotate-4 lg:-rotate-4 lg:flex-1 xl:group-hover:-translate-x-24 xl:group-hover:rotate-4 lg:transition-transform duration-300" />
+          </div>
+
+          <span className="hidden lg:flex items-center gap-x-2 self-center uppercase tracking-widest text-xs text-neutral-600">
+            <img
+              src={sparkleIcon}
+              alt=""
+              aria-hidden="true"
+              className="w-3 h-3"
+            />
+            Hover to fan
+            <img
+              src={sparkleIcon}
+              alt=""
+              aria-hidden="true"
+              className="w-3 h-3"
+            />
+          </span>
         </section>
 
         <GuideCards />
